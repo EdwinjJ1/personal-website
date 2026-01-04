57f08fbf4e5d8159b8a1bc8c5ceffae54e65bd0b
Evanlin <2967329593@qq.com>
2026-01-05 01:55:21 +1100

    fix: remove basePath for custom domain

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: '/personal-website',
-  assetPrefix: '/personal-website/',
   eslint: {
     ignoreDuringBuilds: true,
   },
