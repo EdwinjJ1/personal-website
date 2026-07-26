7524ea16af5b215b4735b6a95960e6e863797013
Evanlin <2967329593@qq.com>
2026-07-27 01:25:40 +1000

    feat(friends): add AIOVTUE-雪 friend link

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -12,4 +12,10 @@ export const friends: Friend[] = [
     link: 'https://joyehuang.me/',
     avatar: 'https://joyehuang.me/favicon/favicon.ico',
   },
+  {
+    name: 'AIOVTUE-雪',
+    desc: '雨滴会记录生命中的每一个瞬间',
+    link: 'https://daily.yybb.us/',
+    avatar: 'https://r2tc.20030327.xyz/file/博客/主题/1780655293662_avatar_me.jpg.PNG',
+  },
 ];
